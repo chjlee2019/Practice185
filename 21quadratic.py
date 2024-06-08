2d2dab6a2fbb71c8ad5ee56778bcda0dcfcad222
chjlee2019 <60269793+chjlee2019@users.noreply.github.com>
2024-06-08 13:23:44 -0700

    update

--- 21quadratic.py
+++ 21quadratic.py
@@ -1,18 +1,10 @@
 # Solve quadratic equation.
-# Two separate functions since I could not find a plus and minus operator.
 
 import math
 
-def quadratic (a, b, c):
-	return (-b + (b**2 + math.sqrt(4*a*c)))/(2*a)
+def quadratic(a, b, c):
+	return (-b + (b**2 + math.sqrt(4*a*c))/(2*a), (-b - (b**2 + math.sqrt(4*a*c))/(2*a)
 	
-print(quadratic(2, 3, 6))		#x1
+print (quadratic(2, 3, 6))		#x1
 print (quadratic(4, 6, 7))		#x2
-print (quadratic (5, 7, 9))		#x3
-
-def quadratic (a, b, c):
-	return (-b + (b**2 - math.sqrt(4*a*c)))/(2*a)
-
-print(quadratic(2, 3, 6))		#x1
-print (quadratic(4, 6, 7))		#x2
-print (quadratic (5, 7, 9))		#x3
+print (quadratic(5, 7, 9))		#x3
