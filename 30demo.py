950c3047d187754cfd55f509db47b2bd7006ed9c
chjlee2019 <60269793+chjlee2019@users.noreply.github.com>
2024-06-08 18:50:15 -0700

    upload

---
+++ 30demo.py
@@ -0,0 +1,33 @@
+#while True:
+#	print ('hello')
+	
+i = 0
+while True:
+	i = i + 1
+	print('hey', i)
+	if i ==3: break
+	
+i = 0
+while i < 3:
+	print(i)
+	i = i + 1
+	print('final value of i is', i)
+	
+i = 1
+while i < 10:
+	print(i)
+	i = i + 3
+	print('final value of i is', i)
+	
+for i in range (1, 10, 3):
+	print(i)
+	
+for i in range(0, 5):
+	print(i)
+
+for char in 'hello':
+	print(char)
+
+seq = 'ACCTTGA'
+for nt in seq:
+	print(nt)
