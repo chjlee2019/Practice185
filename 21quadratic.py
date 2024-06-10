7c63f923ef82e5823379962065e036c3141e0c37
chjlee2019 <60269793+chjlee2019@users.noreply.github.com>
2024-06-10 11:51:19 -0700

    update

--- 21quadratic.py
+++ 21quadratic.py
@@ -3,8 +3,8 @@
 import math
 
 def quadratic(a, b, c):
-	return (-b + (b**2 + math.sqrt(4*a*c))/(2*a), (-b - (b**2 + math.sqrt(4*a*c))/(2*a)
+	return -b + (b**2 + math.sqrt(4*a*c))/(2*a), -b - (b**2 + math.sqrt(4*a*c))/(2*a)
 	
-print (quadratic(2, 3, 6))		#x1
-print (quadratic(4, 6, 7))		#x2
-print (quadratic(5, 7, 9))		#x3
+print(quadratic(2, 3, 6))		#x1
+print(quadratic(4, 6, 7))		#x2
+print(quadratic(5, 7, 9))		#x3
---
+++ 22oligotemp.py
@@ -0,0 +1,8 @@
+# Oligo Temperature (Tm)
+# 
+
+def oligotemp(A, C, G, T):							#define function	
+	if A+C+G+T <= 13: return (A+T)*2 + (G+C)*4
+	else: return 64.9 + 41*(G+C - 16.4) / (A+T+G+C)
+	
+print(oligotemp(2, 3, 3, 2))						#assign variable
