ded6e0dd5ac0fe1fc83a312b94f36c5eafafa877
chjlee2019 <60269793+chjlee2019@users.noreply.github.com>
2024-06-11 09:02:51 -0700

    update

--- 30demo.py
+++ 30demo.py
@@ -32,8 +32,35 @@ seq = 'ACCTTGA'
 for nt in seq:
 	print(nt)
 	
+	
+#	
 for nt1 in 'ACGT':
 	for nt2 in 'ACGT':
 		if nt1 == nt2: print (nt1, nt2, '+1')
 		else:		   print (nt1, nt2, '-1')
-	
+
+nts = 'ACGT'
+for nt1 in nts:
+	for nt2 in nts:
+		if nt1 == nt2: print(nt1, nt2, '+1')
+		else:		   print(nt1, nt2, '-1')
+
+# full matrix: inner loop starts at 0		
+limit = 4
+for i in range(limit):
+	for j in range(0, limit):
+		print(i+1, j+1)
+
+# half matrix + major diagonal: inner loop starts at i		
+limit = 4 
+for i in range(limit):
+	for j in range(i, limit):
+		print(i+1, j+1)
+
+# half matrix + major diagonal: inner loop starts at i
+limit = 4 
+for i in range(limit):
+	for j in range(i+1, limit):
+		print(i+1, j+1)
+
+		
