c4fdc540ed6f18b8f949006a86f141f8b22da4d0
chjlee2019 <60269793+chjlee2019@users.noreply.github.com>
2024-06-20 10:37:20 -0700

    upload

---
+++ 35nchoosek.py
@@ -0,0 +1,27 @@
+# define a factorial function, and call it.
+# n! / k!(n - k)!
+
+"""
+Considerations:
+k or (n-k) =/=0 (denominator can't equal 0')
+n > k: (n-k) > 0 (cannot have negative factorial)
+
+"""
+
+import math
+
+def factorial(n):					#function
+	if n == 0: return 1
+	fac = 1
+	for i in range(1, n + 1):
+		fac = fac * i
+	return fac
+
+
+def nchoosek(n,k):
+	return factorial(n) / (factorial(k) * factorial(n-k))
+	
+	
+for n in range(1, 11):
+	for k in range(1, n):
+		print(n, k, nchoosek(n,k))
